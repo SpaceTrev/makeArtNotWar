10e41f8602df44e55c376fcb8ffe12b323fcdb2c
Sergei Velichkin <sergey.velichkin@gmail.com>
2018-05-26 12:29:16 -0600

    fix

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const app = express();
 const passport = require('passport');
 const session = require('express-session');
 const env = require('dotenv').load();
-<<<<<<< HEAD
-
-=======
->>>>>>> 7ed82892802f4fbcfa730f2ec9184be042077ba6
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -25,13 +21,7 @@ const exphbs = require('express-handlebars');
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-<<<<<<< HEAD
 require('./routes/auth-routes.js')(app, passport);
-=======
-// app.get('/', (req, res) => {
-//   res.send('Welcome to Passport with Sequelize');
-// });
->>>>>>> 7ed82892802f4fbcfa730f2ec9184be042077ba6
 require('./config/passport/passport.js')(passport, db.User);
 
 // Routes
@@ -39,12 +29,8 @@ require('./config/passport/passport.js')(passport, db.User);
 require("./routes/html-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
 require("./routes/job-api-routes.js")(app);
-<<<<<<< HEAD
 
 
-=======
-require("./routes/auth-routes.js")(app, passport);
->>>>>>> 7ed82892802f4fbcfa730f2ec9184be042077ba6
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
